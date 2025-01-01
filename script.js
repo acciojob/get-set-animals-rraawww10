2dd59bbc46b027d007aed0f3817ec19bc57a2403
Rishuraj Singh <102401151+rraawww10@users.noreply.github.com>
2025-01-01 13:00:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-//complete this code
-class Animal {}
+class Animal {
+    constructor(species) {
+        this._species = species; // Use a private property with underscore
+    }
 
-class Dog extends Animal {}
+    get species() {
+        return this._species;
+    }
 
-class Cat extends Animal {}
+    makeSound() {
+        console.log("This animal makes a sound.");
+    }
+}
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+class Cat extends Animal {
+    constructor(species) {
+        super(species); // Call the parent class constructor
+    }
+
+    purr() {
+        console.log("purr");
+    }
+}
+
+class Dog extends Animal {
+    constructor(species) {
+        super(species); // Call the parent class constructor
+    }
+
+    bark() {
+        console.log("woof");
+    }
+}
+
+// Example usage
+const myCat = new Cat("Feline");
+console.log(myCat.species); // Output: Feline
+myCat.makeSound(); // Output: This animal makes a sound.
+myCat.purr(); // Output: purr
+
+const myDog = new Dog("Canine");
+console.log(myDog.species); // Output: Canine
+myDog.makeSound(); // Output: This animal makes a sound.
+myDog.bark(); // Output: woof
